36c02dfbb1a3596000f8489be6426451fe10aa21
AnggaWikaNugraha <anggawika18@gmail.com>
2025-08-15 14:47:27 +0700

    fix: underline navbar aktiv

--- src/app/components/navbar/index.tsx
+++ src/app/components/navbar/index.tsx
@@ -1,21 +1,39 @@
 // src/components/Navbar.tsx
 'use client';
-import Link from 'next/link';
+import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 
 const Navbar = () => {
+  const pathname = usePathname();
+
   return (
     <nav className="fixed top-0 left-0 right-0 z-50 bg-gray-950 border-b border-gray-800 text-white px-6 py-4">
-      <div className="max-w-6xl mx-auto flex items-center justify-between">
-        <Link href="/" className="text-xl font-semibold">
-          Home
+    <div className="max-w-6xl mx-auto flex items-center justify-between">
+      <Link href="/" className={`text-xl font-semibold ${pathname === '/' ? 'underline' : ''}`}>
+        Home
+      </Link>
+      <div className="space-x-6 text-sm">
+        <Link
+          href="/pages/blog"
+          className={`hover:text-gray-400 ${pathname === '/pages/blog' ? 'underline' : ''}`}
+        >
+          Blog
         </Link>
-        <div className="space-x-6 text-sm">
-          <Link href="/pages/blog" className="hover:text-gray-400">Blog</Link>
-          <Link href="/pages/projects" className="hover:text-gray-400">Projects</Link>
-          <Link href="/pages/about" className="hover:text-gray-400">About</Link>
-        </div>
+        <Link
+          href="/pages/projects"
+          className={`hover:text-gray-400 ${pathname === '/pages/projects' ? 'underline' : ''}`}
+        >
+          Projects
+        </Link>
+        <Link
+          href="/pages/about"
+          className={`hover:text-gray-400 ${pathname === '/pages/about' ? 'underline' : ''}`}
+        >
+          About
+        </Link>
       </div>
-    </nav>
+    </div>
+  </nav>
   );
 };
 
